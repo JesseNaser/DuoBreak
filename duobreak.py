936f4f73ae01390035a00f65fbc2b87da80731f0
Jesse Charlie <jc@wayne.edu>
2023-04-13 02:39:41 -0400

    DuoBreak

---
+++ duobreak.py
@@ -0,0 +1,365 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA512
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Util.Padding import pad, unpad
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from getpass import getpass
+from PIL import Image
+from pyzbar.pyzbar import decode as pyzbar_decode
+import atexit
+import base64
+import datetime
+import email.utils
+import json
+import logging
+import os
+import pyotp
+import requests
+import sys
+import time
+import urllib.parse
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class DuoAuthenticator:
+    def __init__(self, config_file="config.json"):
+        self.config_file = config_file
+        self.config = {}
+        self.encryption_key = None
+        self.load_config()
+
+        if "keys" not in self.config:
+            self.config["keys"] = {}
+            self.save_config()
+
+    def derive_encryption_key(self, password, salt=None):
+        if salt is None:
+            salt = os.urandom(16)
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+            backend=default_backend()
+        )
+        return salt, kdf.derive(password.encode('utf-8'))
+
+    def verify_encryption_key(self, key, password, salt):
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+            backend=default_backend()
+        )
+        try:
+            kdf.verify(password.encode('utf-8'), key)
+            return True
+        except Exception:
+            return False
+
+    def encrypt_data(self, data, key):
+        cipher = AES.new(key, AES.MODE_CBC)
+        return cipher.iv + cipher.encrypt(pad(data, AES.block_size))
+
+    def decrypt_data(self, encrypted_data, key):
+        iv = encrypted_data[:AES.block_size]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        return unpad(cipher.decrypt(encrypted_data[AES.block_size:]), AES.block_size)
+
+    def get_password(self, prompt):
+        password = getpass(prompt)
+        if len(password) < 8:
+            logger.error("Password must be at least 8 characters long.")
+            return self.get_password(prompt)
+        return password
+
+    def load_config(self):
+        attempts = 0
+        while attempts < 3:
+            if os.path.exists(self.config_file):
+                print("Ready for password input.")
+                password = self.get_password("Enter the password to unlock your vault: ")
+                with open(self.config_file, "rb") as f:
+                    salt, encrypted_data = f.read(16), f.read()
+                    self.encryption_key = self.derive_encryption_key(password, salt)[1]
+                    if self.verify_encryption_key(self.encryption_key, password, salt):
+                        decrypted_data = self.decrypt_data(encrypted_data, self.encryption_key)
+                        self.config = json.loads(decrypted_data)
+                        break
+                    else:
+                        logger.error("Incorrect password. Please try again.")
+                        attempts += 1
+            else:
+                print("Ready for password input.")
+                password = self.get_password("Enter a password to create a new vault: ")
+                salt, self.encryption_key = self.derive_encryption_key(password)
+                self.config = {"keys": {}}
+                self.save_config(salt)
+        if attempts >= 3:
+            logger.error("Reached maximum password attempts. Exiting...")
+            sys.exit(1)
+
+    def save_config(self, salt=None):
+        if salt is None:
+            with open(self.config_file, "rb") as f:
+                salt = f.read(16)
+        with open(self.config_file, "wb") as f:
+            encrypted_data = self.encrypt_data(json.dumps(self.config).encode("utf-8"), self.encryption_key)
+            f.write(salt + encrypted_data)
+
+    def import_key(self, keyfile):
+        try:
+            self.pubkey = RSA.import_key(keyfile.encode('utf-8'))
+        except ValueError:
+            with open(keyfile, "rb") as f:
+                self.pubkey = RSA.import_key(f.read())
+
+    def prompt_qr_code(self):
+        print("Please provide the QR code image file path (leave empty to cancel):")
+        file_path = input().strip()
+        if not file_path:
+            return None
+        if os.path.exists(file_path):
+            return file_path
+        else:
+            print("Invalid file path. Please try again.")
+        return self.prompt_qr_code()
+
+    def parse_qr_code(self, file_path):
+        img = Image.open(file_path)
+
+        decoded_data = pyzbar_decode(img)
+        if decoded_data:
+            qr_data = decoded_data[0].data.decode()
+            code, host = map(lambda x: x.strip("<>"), qr_data.split("-"))
+            missing_padding = len(host) % 4
+            if missing_padding:
+                host += "=" * (4 - missing_padding)
+            return code, base64.b64decode(host.encode("ascii")).decode("ascii")
+        else:
+            print("Error: Could not decode the QR code. Please try again.")
+            return None
+
+    def activate(self, code, host):
+        url = f"https://{host}/push/v2/activation/{code}?customer_protocol=1"
+        headers = {
+            "User-Agent": "okhttp/2.7.5"
+        }
+        key_pair = RSA.generate(2048)
+        pubkey_data = key_pair.publickey().export_key("PEM").decode('ascii')
+        privkey_data = key_pair.export_key("PEM").decode('ascii')
+        data = {
+            "pubkey": pubkey_data,
+            "pkpush": "rsa-sha512"
+        }
+
+        r = requests.post(url, headers=headers, data=data)
+        response = r.json()
+
+        if "response" in response:
+            return response["response"], pubkey_data, privkey_data
+        else:
+            print("Error during activation. Please try again.")
+            print(f"Server response: {response}")
+            sys.exit(1)
+
+    def generate_signature(self, method, path, time, data, key_config):
+        message = (time + "\n" + method + "\n" + key_config["host"].lower() + "\n" +
+                   path + '\n' + urllib.parse.urlencode(data)).encode('ascii')
+
+        h = SHA512.new(message)
+        signature = pkcs1_15.new(self.pubkey).sign(h)
+        auth = ("Basic " + base64.b64encode((key_config["response"]["pkey"] + ":" +
+                                             base64.b64encode(signature).decode('ascii')).encode('ascii')).decode('ascii'))
+        return auth
+
+    def get_transactions(self, key_config):
+        dt = datetime.datetime.utcnow()
+        time = email.utils.format_datetime(dt)
+        path = "/push/v2/device/transactions"
+        data = {"akey": key_config["response"]["akey"], "fips_status": "1",
+                "hsm_status": "true", "pkpush": "rsa-sha512"}
+
+        signature = self.generate_signature("GET", path, time, data, key_config)
+        r = requests.get(f"https://{key_config['host']}{path}", params=data, headers={
+            "Authorization": signature, "x-duo-date": time, "host": key_config['host']})
+
+        return r.json()
+
+    def reply_transaction(self, transaction_id, answer, key_config):
+        dt = datetime.datetime.utcnow()
+        time = email.utils.format_datetime(dt)
+        path = "/push/v2/device/transactions/" + transaction_id
+        data = {"akey": key_config["response"]["akey"], "answer": answer, "fips_status": "1",
+                "hsm_status": "true", "pkpush": "rsa-sha512"}
+
+        signature = self.generate_signature("POST", path, time, data, key_config)
+        r = requests.post(f"https://{key_config['host']}{path}", data=data, headers={
+            "Authorization": signature, "x-duo-date": time, "host": key_config['host'], "txId": transaction_id})
+
+        return r.json()
+
+    def approve_push_notifications(self, key_name):
+        key_config = self.config["keys"][key_name]
+        self.import_key(key_config["privkey"])
+
+        if "response" in key_config:
+            failed_attempts = 0
+            while True:
+                if failed_attempts >= 10:
+                    print("Reached 10 failed attempts. Returning to previous menu...")
+                    break
+
+                try:
+                    r = self.get_transactions(key_config)
+                except requests.exceptions.ConnectionError:
+                    print("Connection Error")
+                    time.sleep(5)
+                    continue
+
+                approved = False
+
+                if "response" in r and "transactions" in r["response"]:
+                    transactions = r["response"]["transactions"]
+                    print("Checking for transactions")
+                    if transactions:
+                        for tx in transactions:
+                            print(tx)
+                            reply = self.reply_transaction(tx["urgid"], 'approve', key_config)
+                            approved = True
+                            break
+                        if approved:
+                            print("Transaction approved. Returning to previous menu...")
+                            break
+                    else:
+                        print("No transactions")
+                        failed_attempts += 1
+                else:
+                    print(f"Error fetching transactions. Server response: {r}. Retrying...")
+                    failed_attempts += 1
+
+                time.sleep(10)
+        else:
+            print("Error: Activation response is missing. Please try again.")
+            sys.exit(1)
+
+    def show_recent_hotp_codes(self, key_name, count=10):
+        if "hotp_log" in self.config["keys"][key_name]:
+            recent_codes = self.config["keys"][key_name]["hotp_log"][-count:]
+            print(f"Last {count} HOTP codes for {key_name}:")
+            for code in recent_codes:
+                print(code)
+        else:
+            print(f"No recent HOTP codes found for {key_name}.")
+
+    def authenticate(self, key_name):
+        if key_name in self.config["keys"]:
+            print("Select authentication method:")
+            print("1. Duo Push")
+            print("2. HOTP")
+            print("3. Show recent HOTP codes")
+            auth_method = input("Enter the number (1, 2, or 3) corresponding to the method you want to use: ")
+
+            if auth_method == "1":
+                self.approve_push_notifications(key_name)
+            elif auth_method == "2":
+                response = self.config["keys"][key_name]["response"]
+                hotp_secret = base64.b32encode(response["hotp_secret"].encode("ascii")).decode("ascii")
+                hotp = pyotp.HOTP(hotp_secret)
+
+                if "hotp_counter" in self.config["keys"][key_name]:
+                    self.config["keys"][key_name]["hotp_counter"] += 1
+                else:
+                    self.config["keys"][key_name]["hotp_counter"] = 1
+
+                if "hotp_log" not in self.config["keys"][key_name]:
+                    self.config["keys"][key_name]["hotp_log"] = []
+
+                hotp_code = hotp.at(self.config["keys"][key_name]["hotp_counter"])
+                print(f"Generated HOTP code: {hotp_code}")
+
+                self.config["keys"][key_name]["hotp_log"].append(f"{datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')} ({key_name}): {hotp_code}")
+                self.save_config()
+            elif auth_method == "3":
+                self.show_recent_hotp_codes(key_name)
+            else:
+                print("Invalid input. Please try again.")
+                self.authenticate(key_name)
+        else:
+            print("Error: Key not found. Please try again.")
+            sys.exit(1)
+
+    def main_menu(self):
+        while True:
+            print("\nMain Menu:")
+            print("1. Add a new key")
+            print("2. Delete a key")
+            print("3. List keys")
+            print("4. Authenticate")
+            print("5. Exit")
+
+            choice = input("Enter the number (1, 2, 3, 4, or 5) corresponding to the action you want to perform: ")
+
+            if choice == "1":
+                while True:
+                    key_name = input("Enter a nickname for the new key (leave empty to cancel): ").strip()
+                    if not key_name:
+                        print("Returning to the main menu.")
+                        break
+                    if key_name not in self.config["keys"]:
+                        qr_file_path = self.prompt_qr_code()
+                        if qr_file_path is not None:
+                            parsed_data = self.parse_qr_code(qr_file_path)
+                            if parsed_data is not None:
+                                code, host = parsed_data
+                                response, pubkey, privkey = self.activate(code, host)
+                                self.config["keys"][key_name] = {"code": code, "host": host, "response": response, "pubkey": pubkey, "privkey": privkey}
+                                self.save_config()
+                                print(f"Key '{key_name}' added successfully.")
+                            else:
+                                print("Could not add the key. Returning to the main menu.")
+                        else:
+                            print("No QR code file provided. Returning to the main menu.")
+                        break
+                    else:
+                        print("Error: Key with the same name already exists. Please try again.")
+            elif choice == "2":
+                key_name = input("Enter the nickname of the key you want to delete: ")
+                if key_name in self.config["keys"]:
+                    del self.config["keys"][key_name]
+                    self.save_config()
+                    print(f"Key '{key_name}' deleted successfully.")
+                else:
+                    print("Error: Key not found. Please try again.")
+            elif choice == "3":
+                print("Keys:")
+                for key_name in self.config["keys"]:
+                    print(f"- {key_name} ({self.config['keys'][key_name]['response']['customer_name']})")
+            elif choice == "4":
+                key_name = input("Enter the nickname of the key you want to use for authentication: ")
+                if key_name in self.config["keys"]:
+                    self.authenticate(key_name)
+                else:
+                    print("Error: Key not found. Please try again.")
+            elif choice == "5":
+                print("Exiting...")
+                self.save_config()
+                sys.exit(0)
+            else:
+                print("Invalid input. Please try again.")
+
+    def delete_encryption_key_from_memory(self):
+        self.encryption_key = None
+
+if __name__ == "__main__":
+    authenticator = DuoAuthenticator()
+    atexit.register(authenticator.delete_encryption_key_from_memory)
+    authenticator.main_menu()
---
+++ readme.md
@@ -0,0 +1,53 @@
+# DuoBreak
+
+DuoBreak is a Python-based solution for handling Duo authentication on any computer that can run Python. It eliminates the need for a dedicated security key for your computer by emulating your phone's Duo push notifications and HOTP codes.
+
+This tool is perfect for those who want to have a more flexible authentication method without being tied to a specific device or security key.
+
+## Features
+
+- Emulates Duo push notifications and HOTP codes
+- Compatible with any computer that can run Python
+- Secure storage of your authentication keys
+
+## Video Tutorial
+
+[![DuoBreak Video Tutorial](https://img.youtube.com/vi/PLACEHOLDER_VIDEO_ID/0.jpg)](https://www.youtube.com/watch?v=PLACEHOLDER_VIDEO_ID)
+
+## Step-by-Step Tutorial
+
+1. Clone this repository and install the required Python packages:
+
+    ```
+    git clone https://github.com/JesseNaser/DuoBreak.git
+    cd DuoBreak
+    pip install -r requirements.txt
+    ```
+
+2. Run the `duo_authenticator.py` script:
+
+    ```
+    python duobreak.py
+    ```
+
+3. Follow the on-screen instructions to create a new password-protected vault for storing your authentication keys. (Notice your password is hidden from being displayed in the console).
+
+4. On your computer, go to the Duo webpage and add a new device. Choose "Tablet" and then "Android" as your device type, and click "I have Duo Mobile installed".
+
+5. Save the QR code image given by the webpage.
+
+6. In the DuoBreak script, choose "Add a new key" from the main menu. Enter a nickname for the new key and provide the file path to the saved QR code image.
+
+7. The script will automatically activate the new key and store it securely in your vault.
+
+8. To authenticate, choose "Authenticate" from the main menu and enter the nickname of the key you want to use. You can choose to authenticate using Duo push notifications or HOTP codes.
+
+9. Congratulations! You can now use your Python-based Duo Authenticator on any compatible device!
+
+## License
+
+This project is licensed under the AGPL 3.0 or later license. Please see the [LICENSE](LICENSE) file for more information.
+
+## Disclaimer
+
+This tool is provided "as is" and without any warranty. Use it at your own risk. The author(s) are not responsible for any damage or loss that may occur as a result of using this tool. Always follow the best security practices and consider the risks before using any authentication method.
